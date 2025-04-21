b5f0704ba459b27cb6075ead490ff89ed2fc1cb8
Antonio Queb <cardstillo@gmail.com>
2025-04-21 09:30:50 -0600

    Up

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -4,3 +4,12 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     pedimento_number = fields.Char('Número de Pedimento', size=15)
+
+    def _action_done(self):
+        res = super()._action_done()
+        for ml in self:
+            if ml.pedimento_number:
+                ml.write({'move_id': ml.move_id.id})  # Asegura que move_id está vinculado
+                quants = ml.env['stock.quant']._gather(ml.product_id, ml.location_dest_id, lot_id=ml.lot_id, package_id=ml.result_package_id, owner_id=ml.owner_id, strict=True)
+                quants.write({'pedimento_number': ml.pedimento_number})
+        return res
