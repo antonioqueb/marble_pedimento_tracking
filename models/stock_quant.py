c64c311bcc1a438b3eb2e32499d775d76898d7e3
Antonio Queb <cardstillo@gmail.com>
2025-04-20 20:28:01 -0600

    Up

--- models/stock_quant.py
+++ models/stock_quant.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    pedimento_number = fields.Char('Número de Pedimento', related='lot_id.pedimento_number', store=True)
+    pedimento_number = fields.Char('Número de Pedimento', related='move_line_id.pedimento_number', store=True)
